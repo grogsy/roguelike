aa25e20fc2da235e836ee1188f3bf59fde496eb6
Brian <a.gray.cloud@gmail.com>
2020-05-15 17:20:38 -0400

    make looting single item on floor more convenient

--- components/inventory.py
+++ components/inventory.py
@@ -34,10 +34,6 @@ class Inventory:
                 'message': Message('You cannot carry any more, your inventory is full.', tcod.yellow)
             })
         else:
-            results.append({
-                'item_added': item,
-                'message': Message(f"You pick up the {item.name}.", tcod.lighter_blue)
-            })
     
             # rough draft of implementing stackable item
             if isinstance(item, Stackable):
@@ -47,8 +43,13 @@ class Inventory:
                         break
                 else:
                     self.items.append(item)
+                results.append({'item_added': item, 'message': Message(f"You pick up {item.name} x{item.stack_count}")})
             else:    
                 self.items.append(item)
+                results.append({
+                    'item_added': item,
+                    'message': Message(f"You pick up the {item.name}.", tcod.lighter_blue)
+                })
 
         return results
 
